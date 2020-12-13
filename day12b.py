e62de942febdba43f09b9fc5b39656e32821fd06
tabea <tabea@mbr-targeting.com>
2020-12-13 16:31:20 +0100

    don't need the absolute waypoint

--- day12b.py
+++ day12b.py
@@ -15,30 +15,26 @@ def split_val(x):
 
 def move_wp(direction, instruction):
     if direction == 'S':
-        return waypoint_rel[0], waypoint_rel[1] - instruction
+        return waypoint[0], waypoint[1] - instruction
     elif direction == 'N':
-        return waypoint_rel[0], waypoint_rel[1] + instruction
+        return waypoint[0], waypoint[1] + instruction
     elif direction == 'W':
-        return waypoint_rel[0] - instruction, waypoint_rel[1]
+        return waypoint[0] - instruction, waypoint[1]
     elif direction == 'E':
-        return waypoint_rel[0] + instruction, waypoint_rel[1]
+        return waypoint[0] + instruction, waypoint[1]
 
 
 input = list(map(split_val, open('data/12.txt').read().split('\n')))
 coordinates = (0, 0)
-waypoint_rel = (10, 1)
-waypoint = coordinates[0] + waypoint_rel[0], coordinates[1] + waypoint_rel[1]
+waypoint = (10, 1)
 for direction, instruction in input:
     if direction in 'SNWE':
-        waypoint_rel = move_wp(direction, instruction)
-        waypoint = coordinates[0] + waypoint_rel[0], coordinates[1] + waypoint_rel[1]
+        waypoint = move_wp(direction, instruction)
     elif direction == 'F':
-        coordinates = coordinates[0] + waypoint_rel[0] * instruction, coordinates[1] + waypoint_rel[1] * instruction
-        waypoint = coordinates[0] + waypoint_rel[0], coordinates[1] + waypoint_rel[1]
+        coordinates = coordinates[0] + waypoint[0] * instruction, coordinates[1] + waypoint[1] * instruction
     elif direction == 'L':
         while instruction > 0:
-            waypoint_rel = - waypoint_rel[1], waypoint_rel[0]
+            waypoint = - waypoint[1], waypoint[0]
             instruction -= 90
-        waypoint = coordinates[0] + waypoint_rel[0], coordinates[1] + waypoint_rel[1]
 
 print(np.abs(coordinates[0]) + np.abs(coordinates[1]))
